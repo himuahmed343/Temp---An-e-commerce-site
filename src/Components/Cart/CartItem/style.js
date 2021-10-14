26bcb0384c70d6a7727f578e257ac9dddd7cbcfc
Himu Ahmed <64636115+himuahmed343@users.noreply.github.com>
2021-10-14 15:58:51 +0600

    Cart page upgrade

--- src/Components/Cart/CartItem/style.js
+++ src/Components/Cart/CartItem/style.js
@@ -1,11 +1,8 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export default makeStyles((theme) => ({
-  toolbar: theme.mixins.toolbar,
-  grow: {
+export default makeStyles(() => ({
+    root: {
         flexGrow: 1,
       },
-
-      
 }));
 
